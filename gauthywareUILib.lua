2f03f23854cdf344c3e829a82f64d5ce6e42b867
gauthyy <109173696+gauthyy@users.noreply.github.com>
2023-03-07 21:15:26 +0100

    Update gauthywareUILib.lua

--- gauthywareUILib.lua
+++ gauthywareUILib.lua
@@ -8,6 +8,50 @@ function Library:Create(hubname, gamename, presetColor)
 	
 	local Hub = Instance.new("ScreenGui")
 	local MainFrame = Instance.new("Frame")
+	
+	local function AYXB_fake_script() -- Frame.Drag 
+	local script = Instance.new('LocalScript', MainFrame)
+
+	local UIS = game:GetService("UserInputService")
+	function drag(MainFrame)
+		dragToggle = nil
+		dragSpeed = 0.23
+		dragInput = nil
+		dragStart = nil
+		dragPos = nil
+		function updateInput(input)
+			Delta = input.Position - dragStart
+			Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + Delta.X, startPos.Y.Scale, startPos.Y.Offset + Delta.Y)
+			game:GetService("TweenService"):Create(Frame, TweenInfo.new(0.25), {Position = Position}):Play()
+		end
+		MainFrame.InputBegan:Connect(function(input)
+			if (input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch) and UIS:GetFocusedTextBox() == nil then
+				dragToggle = true
+				dragStart = input.Position
+				startPos = Frame.Position
+				input.Changed:Connect(function()
+					if input.UserInputState == Enum.UserInputState.End then
+						dragToggle = false
+					end
+				end)
+			end
+		end)
+		MainFrame.InputChanged:Connect(function(input)
+			if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+				dragInput = input
+			end
+		end)
+		game:GetService("UserInputService").InputChanged:Connect(function(input)
+			if input == dragInput and dragToggle then
+				updateInput(input)
+			end
+		end)
+	end
+	drag(script.Parent)
+	
+end
+coroutine.wrap(AYXB_fake_script)()
+	
 	local TabHolder = Instance.new("ScrollingFrame")
 	local UICorner = Instance.new("UICorner")
 	
